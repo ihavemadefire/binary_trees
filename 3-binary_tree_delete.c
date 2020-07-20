6e3901b81c64a228c6b9d5b466e45ba5e2acc6a1
Jacob Ide <jacobmeide@gmail.com>
2020-07-20 23:26:00 +0000

    1

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -3,10 +3,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - creates a bt node
- * @parent: parent node
- * @value: value
- * Return: pointer to tree node
+ * binary_tree_delete - creates a bt node
+ * @tree: root of tree
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
