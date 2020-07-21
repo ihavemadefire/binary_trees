33d5b8be40b945f40dbdafce80854f9e5a0dbcee
Jacob Ide <jacobmeide@gmail.com>
2020-07-21 03:03:43 +0000

    0

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - prints postordered binary
+ * @tree: root of tree
+ * @func: function pointer
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
