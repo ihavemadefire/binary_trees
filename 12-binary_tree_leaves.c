d838af21e3ece5508e9dae7cc403dff4dadf3a0b
Jacob Ide <jacobmeide@gmail.com>
2020-07-21 18:22:34 +0000

    0

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - prints binary tree height
+ * @tree: root of tree
+ * Return: incremented count
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t l = 0;
+	size_t r = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	else
+	{
+		l = binary_tree_leaves(tree->right);
+		r = binary_tree_leaves(tree->left);
+		return (l + r);
+	}
+}
