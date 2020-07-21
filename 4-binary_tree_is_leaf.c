4611107eb16b124686e1a7eec99db72c5b960971
Jacob Ide <jacobmeide@gmail.com>
2020-07-21 01:59:54 +0000

    0

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - creates a bt node
+ * @node: root of tree
+ * Return: 0 or 1
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	else
+		return (0);
+}
