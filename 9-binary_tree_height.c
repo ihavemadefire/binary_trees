ae5b44e0b85b8c38e16969258b2ffa612763c0da
Jacob Ide <jacobmeide@gmail.com>
2020-07-21 16:31:27 +0000

    0

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,20 +5,22 @@
 /**
  * binary_tree_height - prints binary tree height
  * @tree: root of tree
- * @func: function pointer
+ * Return: incremented count
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l;
-	size_t r;
+	size_t l = 0;
+	size_t r = 0;
 
 	if (tree == NULL)
 	{
 		return 0;
 	}
-	r = binary_tree_height(tree->right);
-	l = binary_tree_height(tree->left);
+	if (tree->right)
+		r = binary_tree_height(tree->right) + 1;
+	if (tree->left)
+		l = binary_tree_height(tree->left) + 1;
 	if (l > r)
-		return (l + 1);
-	return (r + 1);
+		return (l);
+	return (r);
 }
